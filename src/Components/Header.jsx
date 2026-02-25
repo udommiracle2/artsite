3dec39ef03041abf76b7d730521ef47c4cdb5fb8
udommiracle2 <udommiracle2@gmail.com>
2026-02-25 11:20:48 +0100

    fourth commit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,20 +1,34 @@
-const Header = () => {
-    return (
-        <header className="section">
-            <div className="logo">Art Studio</div>
+import { useState } from "react"
 
-            <nav>
-                <ul>
-                    <li>Home</li>
-                    <li>About Me</li>
-                    <li>Offer</li>
-                    <li>Blog</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
-            <i className="fa-solid fa-cart-shopping"></i>
-        </header>
-    )
+
+ 
+
+const Header = () => {
+        const [menuOpen, setMenuOpen] = useState(false);
+
+        const toggleMenu = () => {
+            setMenuOpen(!menuOpen);
+        };    
+        return (
+            <header className="section">
+                <div className="logo">Art Studio</div>
+
+                <nav className={menuOpen ? "nav-menu active" : "nav-menu"}>
+                    <ul>
+                        <li>Home</li>
+                        <li>About Me</li>
+                        <li>Offer</li>
+                        <li>Blog</li>
+                        <li>Contact</li>
+                    </ul>
+                </nav>
+                <i className="fa-solid fa-cart-shopping"></i>
+                <div className="hamburger" onClick={toggleMenu}>
+                    <i className={menuOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars"}></i>
+                </div>
+            </header>
+
+        )
 }
 
 export default Header
